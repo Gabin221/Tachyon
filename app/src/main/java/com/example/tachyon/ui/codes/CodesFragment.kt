f73c2c885fbc14bf81dfb6a2cc7357313fd9c2ce
Gabin221 <110061374+Gabin221@users.noreply.github.com>
2024-02-28 00:48:31 +0100

    Update CodesFragment.kt

--- app/src/main/java/com/example/tachyon/ui/codes/CodesFragment.kt
+++ app/src/main/java/com/example/tachyon/ui/codes/CodesFragment.kt
@@ -1,33 +1,49 @@
 package com.example.tachyon.ui.codes
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 import androidx.viewpager.widget.ViewPager
-import com.google.android.material.tabs.TabLayout
+//import android.widget.Toolbar
+import androidx.fragment.app.Fragment
 import com.example.tachyon.R
+import com.example.tachyon.databinding.FragmentCodesBinding
+import com.google.android.material.tabs.TabLayout
 
-class MainActivity : AppCompatActivity() {
+class CodesFragment : Fragment() {
+
+    private var _binding: FragmentCodesBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
 
     private lateinit var pager: ViewPager // creating object of ViewPager
-    private lateinit var tab: TabLayout // creating object of TabLayout
-    private lateinit var bar: Toolbar // creating object of ToolBar
+    private lateinit var tab: TabLayout  // creating object of TabLayout
+    private lateinit var bar: Toolbar    // creating object of ToolBar
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+
+        _binding = FragmentCodesBinding.inflate(inflater, container, false)
+        val root: View = binding.root
 
         // set the references of the declared objects above
-        pager = findViewById(R.id.viewPager)
-        tab = findViewById(R.id.tabs)
-        bar = findViewById(R.id.toolbar)
+        pager = binding.root.findViewById(R.id.viewPager)
+        tab = binding.root.findViewById(R.id.tabs)
+        bar = binding.root.findViewById(R.id.toolbar)
 
         // To make our toolbar show the application
         // we need to give it to the ActionBar
-        setSupportActionBar(bar)
+        (requireActivity() as AppCompatActivity).setSupportActionBar(bar)
 
-        // Initializing the ViewPagerAdapter
-        val adapter = ViewPagerAdapter(supportFragmentManager)
+        val adapter = ViewPagerAdapter(childFragmentManager)
 
         // add fragment to the list
         adapter.addFragment(PythonFragment(), "Python")
@@ -39,5 +55,12 @@ class MainActivity : AppCompatActivity() {
 
         // bind the viewPager with the TabLayout.
         tab.setupWithViewPager(pager)
+
+        return root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
